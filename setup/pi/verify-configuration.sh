f02a9115126a2d41205d15f48f3afbc119a11637
Marco Nelissen <marco.nelissen@gmail.com>
2022-07-17 11:20:57 -0700

    Make fewer assumptions about partitions
    Instead of assuming that there are precisely 2 partitions, /boot
    and / in that order, only assume that the root partition is the
    last partition, followed by unpartitioned space.

--- setup/pi/verify-configuration.sh
+++ setup/pi/verify-configuration.sh
@@ -47,25 +47,31 @@ function check_available_space () {
 function check_available_space_sd () {
   setup_progress "Verifying that there is sufficient space available on the MicroSD card..."
 
-  # The following assumes that the root and boot partitions are adjacent at the start
-  # of the disk, and that all the free space is at the end.
-
-  local totalsize
-  local part1size
-  local part2size
-  local available_space
-
-  totalsize=$(blockdev --getsize64 "${BOOT_DEVICE}")
-  part1size=$(blockdev --getsize64 "${BOOT_DEVICE_PART}1")
-  part2size=$(blockdev --getsize64 "${BOOT_DEVICE_PART}2")
-  available_space=$((totalsize - part1size - part2size))
-
-  # Require at least 40 GB of available space.
-  if [ "$available_space" -lt  $(( (1<<30) * 40)) ]
+  # check if backingfiles and mutable already exist
+  if [ -e /dev/disk/by-label/backingfiles ] && [ -e /dev/disk/by-label/mutable ]
   then
-    setup_progress "STOP: The MicroSD card is too small: $available_space bytes available."
-    setup_progress "$(parted "${BOOT_DEVICE}" print)"
-    exit 1
+    backingfiles_size=$(blockdev --getsize64 /dev/disk/by-label/backingfiles)
+    if [ "$backingfiles_size" -lt  $(( (1<<30) * 39)) ]
+    then
+      setup_progress "STOP: Existing backingfiles partition is too small"
+      exit 1
+    fi
+  else
+    # The following assumes that all the partitions are at the start
+    # of the disk, and that all the free space is at the end.
+ 
+    local available_space
+ 
+    # query unpartitioned space
+    available_space=$(sfdisk -F "$BOOT_DISK" | grep -o '[0-9]* bytes' | head -1 | awk '{print $1}')
+ 
+    # Require at least 40 GB of available space.
+    if [ "$available_space" -lt  $(( (1<<30) * 40)) ]
+    then
+      setup_progress "STOP: The MicroSD card is too small: $available_space bytes available."
+      setup_progress "$(parted "${BOOT_DISK}" print)"
+      exit 1
+    fi
   fi
 
   setup_progress "There is sufficient space available."
