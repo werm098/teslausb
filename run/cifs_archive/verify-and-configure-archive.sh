3c53d3dab2f718dc65253379ba8fd9930ce7cd07
Marco Nelissen <marco.nelissen@gmail.com>
2022-11-05 19:05:26 -0700

    setup: "fix" archive server reachable check
    On some systems hping3 inexplicably selects the loopback interface
    when attempting to ping the archive server, instead of the lan
    interface. Work around this by explicitly retrying on the lan interface.

--- run/cifs_archive/verify-and-configure-archive.sh
+++ run/cifs_archive/verify-and-configure-archive.sh
@@ -15,7 +15,11 @@ function log_progress () {
 function check_archive_server_reachable () {
   log_progress "Verifying that the archive server $ARCHIVE_SERVER is reachable..."
   local serverunreachable=false
-  hping3 -c 1 -S -p 445 "$ARCHIVE_SERVER" 1>/dev/null 2>&1 || serverunreachable=true
+  local default_interface
+  default_interface=$(route | grep "^default" | awk '{print $NF}')
+  hping3 -c 1 -S -p 445 "$ARCHIVE_SERVER" 1>/dev/null 2>&1 ||
+    hping3 -c 1 -S -p 445 -I "$default_interface" "$ARCHIVE_SERVER" 1>/dev/null 2>&1 ||
+    serverunreachable=true
 
   if [ "$serverunreachable" = true ]
   then
