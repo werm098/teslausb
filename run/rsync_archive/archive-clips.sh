008c639780ae6fb45b5f84db2be0a1bd662f9181
Marco Nelissen <marco.nelissen@gmail.com>
2023-08-10 19:04:55 -0700

    Log rsync output in case of failure

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -2,8 +2,12 @@
 
 while [ -n "${1+x}" ]
 do
-  rsync -avhRL --timeout=60 --remove-source-files --no-perms --omit-dir-times --stats \
-        --log-file=/tmp/archive-rsync-cmd.log --ignore-missing-args \
-        --files-from="$2" "$1" "$RSYNC_USER@$RSYNC_SERVER:$RSYNC_PATH" &> /tmp/rsynclog || [[ "$?" = "24" ]]
+  if ! (rsync -avhRL --timeout=60 --remove-source-files --no-perms --omit-dir-times \
+        --stats --log-file=/tmp/archive-rsync-cmd.log --ignore-missing-args \
+        --files-from="$2" "$1" "$RSYNC_USER@$RSYNC_SERVER:$RSYNC_PATH" &> /tmp/rsynclog || [[ "$?" = "24" ]] )
+  then
+    cat /tmp/archive-rsync-cmd.log /tmp/rsynclog > /tmp/archive-error.log
+    exit 1
+  fi
   shift 2
 done
